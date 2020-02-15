84f09340b872f42468a8cc98b4a5afd1519d5d55
Fkame <nikitos_flame@mail.ru>
2020-02-15 15:04:58 +0300

    Point3d: distanceTo() + equals() + test

--- Point3d.java
+++ Point3d.java
@@ -31,4 +31,45 @@ public class Point3d {
 	public double getZ() {
 		return this.z;
 	}
+	
+	public void setX(double x) {
+		this.x = x;
+	}
+	
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+	public void setZ(double z) {
+		this.z = z;
+	}
+	
+	public boolean equals(Point3d p) {
+		double x, y, z;
+		x = p.getX();
+		y = p.getY();
+		z = p.getZ();
+		if (this.x == x & this.y == y & this.z == z) return true;
+		else return false;
+	}
+	
+	public double distanceTo(Point3d p) {
+		double x, y, z;
+		x = p.getX();
+		y = p.getY();
+		z = p.getZ();
+		double diffXpow2 = Math.pow(x - this.x, 2);
+		double diffYpow2 = Math.pow(y - this.y, 2);
+		double diffZpow2 = Math.pow(z - this.z, 2);
+		double distance = Math.pow(diffXpow2 + diffYpow2 + diffZpow2, 0.5);
+		return Point3d.roundTo2Symbols(distance);
+	}
+	
+	public static double roundTo2Symbols(double n) {
+		n *= 100;
+		int in = (int)Math.round(n);
+		n = (double) in / 100;
+		return n;
+	}
+	
 }
--- testPoint3d.java
+++ testPoint3d.java
@@ -47,6 +47,49 @@ public class testPoint3d {
 		z1 = p.getZ();
 		testPoint3d.checkingPoint3d(x, y, z, x1, y1, z1, "Point3d(2.5, 10.7, -1)");
 		
+		// Set methods
+		x = -1;
+		y = -2;
+		z = -3.1;
+		p.setX(x);
+		p.setY(y);
+		p.setZ(z);
+		x1 = p.getX();
+		y1 = p.getY();
+		z1 = p.getZ();
+		testPoint3d.checkingPoint3d(x, y, z, x1, y1, z1, "Point3d setter");
+		
+		// Equals testing
+		p = new Point3d(0, 1, 2);
+		Point3d p1 = new Point3d(0.0, 1.1, 2.2);
+		Point3d p2 = new Point3d(0.0, 1.0, 2.0);
+		if (!p.equals(p1) & p.equals(p2) & !p1.equals(p2) & p2.equals(p)) {
+			System.out.println ("Point3d.equals success");
+		} else {
+			System.out.println ("Point3d.equals failed");
+		}
+		
+		// roundTo2Symbols testing
+		double rezult;
+		double num = 2.1234567;
+		double num2 = 3.0;
+		double num3 = 4.1;
+		System.out.println("test Point3d.roundTo2Symbols(): ");
+		System.out.println("\t2.1234567 = " + Point3d.roundTo2Symbols(num));
+		System.out.println("\t3.0 = " + Point3d.roundTo2Symbols(num2));
+		System.out.println("\t4.1 = " + Point3d.roundTo2Symbols(num3));
+		
+		// Point3d.distanceTo() testing
+		System.out.println("test Point3d.distanceTo(): ");
+		Point3d point1 = new Point3d(0, -3, 3);
+		Point3d point2 = new Point3d(3, 1, 3);
+		double trueDistance = 5.0;
+		double distance = point1.distanceTo(point2);
+		System.out.println("\tDistance = " + distance + ", true answer = " + trueDistance);
+		if (distance == trueDistance) System.out.println ("Point3d.distanceTo() success");
+		else System.out.println ("Point3d.distanceTo() failed");
+		
+		
 	}
 	
 	private static void checkingPoint3d(double x, double y, double z, double x1, double y1, double z1, String testMethod) {
